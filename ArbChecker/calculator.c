7f85e0c6930320d1545bd543bfaa71639e127bfb
Jonathon Roberts <jonrob1@hotmail.com>
2017-07-01 00:33:09 +0100

    Added changeodds function

--- ArbChecker/calculator.c
+++ ArbChecker/calculator.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 float setreturn(int noodds, float odds[]);
+void changeodds(int *noodds, float *op);
 
 int main(int argc, char *argv[]){
 	int i;
-	int noodds = argc-1;
-	float odds[noodds];
+	int noodds = argc-1;//number of odds
+	if(noodds<2){
+		printf("Two or more odds required\n");
+		return 0;
+	}
+	float odds[20];
+	float *op;//odds pointer required for changeodds()
+	op = odds;
 	for(i=1;i<argc;i++){
 		odds[i-1] = atof(argv[i]);
 	}
-	float returnodds =setreturn(noodds, odds);
+	float returnodds = setreturn(noodds, odds);
 	float totalstake = 100;
 	int firstrunflag = 0;
 	char str[15];
 	int a;
-	float returncash;
+	float returncash = totalstake * returnodds;
 	while(1){
 		if(firstrunflag == 0){
-			//system("clear");
 			firstrunflag++;
 		}
 		else{
 			fgets(str,15,stdin);
-		}
-		//if only one character is entered a = character entered
-		if(str[2]==0){
-			a = str[0];
-		}
-
-		switch(a){
-			//case: 'c' {changeodds();break;}
-			case 'q': return 0;
-			default: {totalstake =atof(str);
-				  returncash = totalstake*returnodds;
-				  a = 0;
-				 }
+			//if only one character is entered a = character entered
+			if(str[2]==0){
+				a = str[0];
 			}
+			switch(a){
+				case 'c': {changeodds(&noodds,op);
+					  returnodds = setreturn(noodds,odds);
+					  returncash = totalstake*returnodds;break;}
+				case 'q': return 0;
+				default: {totalstake =atof(str);
+						 returncash = totalstake*returnodds;}
+			}
+		}
+		a = 0;
 		//Output
+		system("clear");
+		printf("c - change odds : q - quit\n");
 		printf("Total stake: %.2f\n",totalstake);
 		printf("Total return:  %.2f\n",returncash);
 		for(i = 0;i<noodds;i++){
@@ -47,8 +54,6 @@ int main(int argc, char *argv[]){
 		}
 		printf("Input total stake: ");
 		}
-
-
 	}
 
 float setreturn(int noods, float odds[]){
@@ -58,4 +63,21 @@ float setreturn(int noods, float odds[]){
 		returnodds += (1/odds[i]);
 	}
 	return (1/returnodds);
+}
+
+void changeodds(int *noodds, float *op){
+	char str[60];
+	do{
+		printf("Enter odds seperated by a space: ");
+		*noodds = 0;
+		char *p = str;
+		char tmpword[10];
+		if (fgets(str, sizeof str, stdin) != NULL) {
+			while (sscanf(p, "%s",tmpword ) != -1) {
+				op[(*noodds)++] = atof(tmpword);
+				p+= strlen(tmpword)+1;
+
+			  }
+		}
+	}while((*noodds)<2);
 }
