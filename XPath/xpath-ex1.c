7422e997621a3afac33133894a50ca31396c4746
Jonathon Roberts <jonrob1@hotmail.com>
2017-07-05 17:47:05 +0100

    Added html support

--- XPath/xpath-ex1.c
+++ XPath/xpath-ex1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <libxml/xpath.h>
+#include <libxml/HTMLparser.h>//htmlReadFile
+
 
 /* An example XPath program for returning elements from an XML file */
 
@@ -7,12 +9,24 @@ xmlNodeSetPtr getnodeset(xmlDocPtr doc,xmlChar *xpath);
 
 int main(){
 	xmlInitParser();
+	//char *docname = "riseup.html";
 	char *docname = "Sitemap.xml";
 	//char *expr = "/*:sitemapindex/*:sitemap/*:loc";//xpath2 syntax
 	char *expr = "/*[local-name() = 'sitemapindex']/*[local-name() = 'sitemap']/*[local-name() = 'loc']";
+	//char *expr = "/html/head/title";
 	int i;
 	int size;
 
+	/*
+	For reading html files
+	xmlDocPtr doc = htmlReadFile(docname, NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+
+	figure out how to get curl into the program and then find out what goes there
+
+	xmlDocPtr doc = htmlParseDoc(whatgoeshere, NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+
+	xmlDocPtr doc = xmlParseDoc(whatgoeshere);
+	*/
 	xmlDocPtr doc = xmlParseFile(docname);
 	xmlChar *xpath = (xmlChar*) expr;
 	xmlNodeSetPtr nodeset;
