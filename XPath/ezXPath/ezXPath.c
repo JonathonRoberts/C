a80f11e7d2afc39a7b016bf30f5927016c66e8df
Jonathon Roberts <jonrob1@hotmail.com>
2017-07-08 10:30:05 +0100

    Improved support for multiple calls

--- XPath/ezXPath/ezXPath.c
+++ XPath/ezXPath/ezXPath.c
@@ -4,12 +4,6 @@
 #include "ezcurl.c"
 
 /* An example XPath program for returning elements from an XML file */
-
-xmlNodeSetPtr getnodeset(xmlDocPtr doc,xmlChar *xpath);
-int extern ezXPathXML(char *website,char *expr,char *output[]){
-	//xmlInitParser();
-	int i;
-	int size = 0;
 	/*
 	 *
 	 * xpath1 syntax for ignoring namespace
@@ -29,23 +23,27 @@ int extern ezXPathXML(char *website,char *expr,char *output[]){
 	 * xmlDocPtr doc = htmlReadFile(docname, NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
 	 *
 	 * For downloading and parsing html sites */
-	xmlDocPtr doc = xmlParseDoc(ezcurl(website));
+
+xmlNodeSetPtr getnodeset(xmlDocPtr doc,xmlChar *xpath);
+
+int extern ezXPathXMLFile(char *file,char *expr,char *output[]){
+	xmlInitParser();
+	int i;
+	int size = 0;
+	xmlDocPtr doc = xmlParseFile(file);
 	xmlChar *xpath = (xmlChar*) expr;
 	xmlNodeSetPtr nodeset;
 	xmlXPathObjectPtr result;
 	xmlChar *element;
 	result = getnodeset(doc,xpath);
-	**output = (char **)malloc(1);
 
 	if(result){
 		nodeset = result->nodesetval;
 		size = nodeset->nodeNr;
-		*output = malloc(size);
-		realloc(*output,size);
+		*output=malloc(size);
 		for(i=0;i<size;i++){
 			element = xmlNodeListGetString(doc,nodeset->nodeTab[i]->xmlChildrenNode,1);
 			output[i] = (char *)malloc(strlen(element));
-			printf("%s\n",element);
 			strxfrm(output[i],element,strlen(element)+1);
 			xmlFree(element);
 		}
@@ -53,45 +51,54 @@ int extern ezXPathXML(char *website,char *expr,char *output[]){
 	}
 
 	xmlFreeDoc(doc);
-	//xmlCleanupParser();
+	xmlCleanupParser();
+	return size;
+}
+
+int extern ezXPathXML(char *website,char *expr,char *output[]){
+	xmlInitParser();
+	int i;
+	int size = 0;
+	xmlDocPtr doc = xmlReadDoc(ezcurl(website),website,NULL,1);
+	xmlChar *xpath = (xmlChar*) expr;
+	xmlNodeSetPtr nodeset;
+	xmlXPathObjectPtr result;
+	xmlChar *element;
+	result = getnodeset(doc,xpath);
+
+	if(result){
+		nodeset = result->nodesetval;
+		size = nodeset->nodeNr;
+		*output=malloc(size*4000);
+		for(i=0;i<size;i++){
+			element = xmlNodeListGetString(doc,nodeset->nodeTab[i]->xmlChildrenNode,1);
+			output[i] = (char *)malloc(strlen(element));
+			strxfrm(output[i],element,strlen(element)+1);
+			xmlFree(element);
+		}
+		xmlXPathFreeObject(result);
+	}
+
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
 	return size;
 }
 
 int extern ezXPathHTML(char *website,char *expr,char *output[]){
-	//xmlInitParser();
+	xmlInitParser();
 	int i;
-	int size=0;
-	/*
-	 *
-	 * xpath1 syntax for ignoring namespace
-	 * char *expr = "/*[local-name() = 'sitemapindex']/*[local-name() = 'sitemap']/*[local-name() = 'loc']";
-	 * xpath2 syntax for ignoring namespace
-	 * char *expr = "/*:sitemapindex/*:sitemap/*:loc";
-	 *
-	 * char *docname = "Sitemap.xml";
-	 *
-	 * For downloading and parsing xml websites
-	 * xmlDocPtr doc = xmlParseDoc(ezcurl("https://www.sitemaps.org/sitemap.xml");
-	 *
-	 * For parsing xml files
-	 * xmlDocPtr doc = xmlParseFile(docname);
-	 *
-	 * For parsing html files
-	 * xmlDocPtr doc = htmlReadFile(docname, NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
-	 *
-	 * For downloading and parsing html sites */
+	int size = 0;
 	xmlDocPtr doc = htmlReadDoc(ezcurl(website),website,NULL, HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET | HTML_PARSE_IGNORE_ENC );
 	xmlChar *xpath = (xmlChar*) expr;
 	xmlNodeSetPtr nodeset;
 	xmlXPathObjectPtr result;
 	xmlChar *element;
 	result = getnodeset(doc,xpath);
-	*output = (char *)malloc(1);
 
 	if(result){
 		nodeset = result->nodesetval;
 		size = nodeset->nodeNr;
-		**output = (char *)malloc(size);
+		*output=malloc(size);
 		for(i=0;i<size;i++){
 			element = xmlNodeListGetString(doc,nodeset->nodeTab[i]->xmlChildrenNode,1);
 			output[i] = (char *)malloc(strlen(element));
@@ -102,27 +109,55 @@ int extern ezXPathHTML(char *website,char *expr,char *output[]){
 	}
 
 	xmlFreeDoc(doc);
-	//xmlCleanupParser();
+	xmlCleanupParser();
 	return size;
 }
 
+int extern ezXPathHTMLFile(char *file,char *expr,char *output[]){
+	xmlInitParser();
+	int i;
+	int size = 0;
+	 xmlDocPtr doc = htmlReadFile(file, NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+	xmlChar *xpath = (xmlChar*) expr;
+	xmlNodeSetPtr nodeset;
+	xmlXPathObjectPtr result;
+	xmlChar *element;
+	result = getnodeset(doc,xpath);
+
+	if(result){
+		nodeset = result->nodesetval;
+		size = nodeset->nodeNr;
+		*output=malloc(size);
+		for(i=0;i<size;i++){
+			element = xmlNodeListGetString(doc,nodeset->nodeTab[i]->xmlChildrenNode,1);
+			output[i] = (char *)malloc(strlen(element));
+			strxfrm(output[i],element,strlen(element)+1);
+			xmlFree(element);
+		}
+		xmlXPathFreeObject(result);
+	}
+
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
+	return size;
+}
 xmlNodeSetPtr getnodeset(xmlDocPtr doc,xmlChar *xpath){
 	xmlXPathObjectPtr result;
 	xmlXPathContextPtr context;
 	context = xmlXPathNewContext(doc);
 	if(context==NULL){
-		printf("Error in xmlXpathNewContext\n");
+		/*printf("Error in xmlXpathNewContext\n");*/
 		return NULL;
 	}
 	result = xmlXPathEvalExpression(xpath,context);
 	xmlXPathFreeContext(context);
 	if(result==NULL){
-		printf("Error in xmlXpathEvalExpression\n");
+		/*printf("Error in xmlXpathEvalExpression\n");*/
 		return NULL;
 	}
 	if(xmlXPathNodeSetIsEmpty(result->nodesetval)){
 		xmlXPathFreeObject(result);
-		printf("No Results\n");
+		/*printf("No Results\n");*/
 		return NULL;
 	}
 	return result;
--- XPath/ezXPath/main.c
+++ XPath/ezXPath/main.c
@@ -2,9 +2,8 @@
 #include "ezXPath.c"
 
 int main(){
-	xmlInitParser();
-
-	char *output[0];
+	char **output;
+	**output = malloc(0);
 	int i;
 	int size = 0;
 	size = ezXPathHTML("example.com","/html/head/title",output);
@@ -15,16 +14,35 @@ int main(){
 			free(output[i]);
 		}
 	}
-	size = ezXPathHTML("example.com","/html/head/title",output);
+	printf("%s",output);
+	size = ezXPathHTMLFile("example.com","/html/head/title",output);
 	if(size!=0){
 		printf("Found %d elements:\n",size);
 		for(i =0;i<size;i++){
 			printf("%s\n",output[i]);
 			free(output[i]);
 		}
+	}
+	size = ezXPathHTMLFile("example.com","/html/head/title",output);
+	if(size!=0){
+		printf("Found %d elements:\n",size);
+		for(i =0;i<size;i++){
+			printf("%s\n",output[i]);
+			free(output[i]);
+		}
+	}
+	output = malloc(40000);
+
+	size = ezXPathXML("https://www.sitemaps.org/sitemap.xml","/*[local-name() = 'urlset']/*[local-name() = 'url']/*[local-name() = 'loc']",output);
+	if(size!=0){
+		printf("Found %d elements:\n",size);
+		for(i =0;i<size;i++){
+			printf("%s\n",output[i]);
+			free(output[i]);
+		}
+
 	}
 	size = ezXPathXML("https://www.sitemaps.org/sitemap.xml","/*[local-name() = 'urlset']/*[local-name() = 'url']/*[local-name() = 'loc']",output);
-	//*output = realloc(*output,size);
 	if(size!=0){
 		printf("Found %d elements:\n",size);
 		for(i =0;i<size;i++){
@@ -34,6 +52,5 @@ int main(){
 	}
 
 
-	xmlCleanupParser();
 	return 1;
 }
