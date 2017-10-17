6068a5eef92e559378ac185271f5a73963eddf4e
Jonathon Roberts <jon@jonrob.net>
2017-10-17 21:40:04 +0100

    New

---
+++ X11/pac/pac.c
@@ -0,0 +1,114 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Global X variables */
+Display *dis;
+int screen;
+Window win;
+GC gc;
+
+XGCValues gr_values;
+XColor color,dummy;
+XFontStruct *fontinfo;
+
+void init_x();
+void close_x();
+void redraw();
+void changecoln(char *col);
+void changecolrgb(unsigned short r,unsigned short g,unsigned short b);
+void changeffontsize(char *font);
+
+
+int main () {
+
+	XEvent event;
+	KeySym key;
+	char text[255];
+	int px =200;
+	int py =200;
+
+	init_x();
+
+	while(1) {
+
+		changeffontsize("-*-helvetica-*-r-*-*-24-*-*-*-*-*-*-*");
+		changecoln("black");
+
+		int i;
+		double si;
+		unsigned short r,g,b = 0;
+			changecolrgb(-1,-1,0);
+			changecolrgb(0,0,0);
+			while(1){
+		for(i=0;i<60;i++){
+		i = 30;
+			redraw();
+			XDrawArc( dis, win, gc, px, py,  20,20, 45, (300+i)*64);
+			usleep(1);
+		}
+		for(;i>1;i--){
+		i = 30;
+			redraw();
+			XDrawArc( dis, win, gc, px, py,  20,20, 45, (300+i)*64);
+			usleep(1);
+		}
+			}
+	}
+}
+
+void init_x() {
+/* get the colors black and white */
+	unsigned long black,white;
+
+	dis=XOpenDisplay((char *)0);
+	if (dis == NULL){
+		fprintf(stderr, "Cannot open display\n");
+		exit (1);
+	}
+   	screen=DefaultScreen(dis);
+	black=BlackPixel(dis,screen),
+	white=WhitePixel(dis, screen);
+   	win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,
+		640, 480, 5,black, white);
+	XSetStandardProperties(dis,win,"XLib Demo","Demo",None,NULL,0,NULL);
+	XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
+        gc=XCreateGC(dis, win, 0,0);
+	XSetBackground(dis,gc,white);
+	XSetForeground(dis,gc,black);
+	XClearWindow(dis, win);
+	XMapRaised(dis, win);
+};
+
+void close_x() {
+	XFreeGC(dis, gc);
+	XDestroyWindow(dis,win);
+	XCloseDisplay(dis);
+	exit(1);
+};
+
+void redraw() {
+	XClearWindow(dis, win);
+};
+
+void changecoln(char *col){
+	XAllocNamedColor(dis, DefaultColormap(dis,screen),col,&color,&dummy);
+	gr_values.foreground = color.pixel;
+	gc = XCreateGC(dis,win,GCFont+GCForeground, &gr_values);
+}
+void changecolrgb(unsigned short r,unsigned short g,unsigned short b){
+	color.red=r;
+	color.green=g;
+	color.blue=b;
+	XAllocColor(dis, DefaultColormap(dis,screen),&color);
+	gr_values.foreground = color.pixel;
+	gc = XCreateGC(dis,win,GCFont+GCForeground, &gr_values);
+}
+void changeffontsize(char *font){
+	fontinfo = XLoadQueryFont(dis,font);
+	gr_values.font = fontinfo->fid;
+	gc = XCreateGC(dis,win,GCFont+GCForeground, &gr_values);
+}
